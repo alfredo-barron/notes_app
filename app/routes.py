ef79ba720a6a62c34f49027f939cd569bf975973
alfredo-barron <alfreedobarron@gmail.com>
2024-10-03 22:23:49 -0600

    First commit

---
+++ app/routes.py
@@ -0,0 +1,37 @@
+from flask import render_template, redirect, url_for, request
+from app import db
+from app.models import Note
+from flask import current_app as app
+
+@app.route('/')
+def index():
+    notes = Note.query.all()
+    return render_template('index.html', notes=notes)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_note():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        new_note = Note(title=title, content=content)
+        db.session.add(new_note)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add_note.html')
+
+@app.route('/edit/<int:note_id>', methods=['GET', 'POST'])
+def edit_note(note_id):
+    note = Note.query.get_or_404(note_id)
+    if request.method == 'POST':
+        note.title = request.form['title']
+        note.content = request.form['content']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit_note.html', note=note)
+
+@app.route('/delete/<int:note_id>')
+def delete_note(note_id):
+    note = Note.query.get_or_404(note_id)
+    db.session.delete(note)
+    db.session.commit()
+    return redirect(url_for('index'))
